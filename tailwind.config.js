ad55747376389b705933a59881e0373de4aec26c
Ayush01010101 <ayushawachar0989@gmail.com>
2024-07-22 15:12:23 +0530

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     
 
 
-    extend: {},
+    extend: {
+      boxShadow: {
+        'white-glow': '0 0 10px 2px rgba(255, 255, 255, 0.75)',
+      },
+
+    },
   },
   plugins: [],
 }
